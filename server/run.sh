671c3a55fd7c8b987699bc25007de08d4baa3fb2
Julien Chavanton <jchavanton@gmail.com>
2024-02-02 15:31:27 -0500

    hct files

---
+++ server/run.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+DIR_PREFIX=`pwd`
+NAME="hct_server"
+IMAGE=${NAME}:latest
+CONTAINER=${NAME}
+
+# docker ps | grep '${CONTAINER_NAME}' | cut -d ' ' -f 1 | xargs --no-run-if-empty docker kill || /usr/bin/true;
+# docker container ls -a | grep ${CONTAINER_NAME} | cut -d ' ' -f 1 | xargs --no-run-if-empty docker container rm || /usr/bin/true;
+
+docker stop ${CONTAINER}
+docker rm ${CONTAINER}
+docker run -d --net=host --name=${CONTAINER} \
+	-v ${PWD}/xml:/xml \
+	${IMAGE}
