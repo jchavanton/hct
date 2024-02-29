66daca739d154c55adc799e89507716d6739bda9
Julien Chavanton <jchavanton@gmail.com>
2024-02-28 19:11:37 -0500

    freeswitch config update

--- server/freeswitch/run.sh
+++ server/freeswitch/run.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
-VERSION=0.0.1
+VERSION=0.0.0
 DIR_PREFIX=`pwd`
+IMAGE=freeswitch
 
-if [ -z "${1}" ]; then
-  echo >&2 "No tag was provided to run. Usage: ./run.sh GIT_COMMIT_ID_GOES_HERE"
-  exit 2
-fi
-
-IMAGE="848116219031.dkr.ecr.us-west-1.amazonaws.com/com.connectingpointpro.webrtc-gw-freeswitch:${1}"
-
-## For local-dev stop things. This is already done by CodeDeploy but its safe to re-run
-docker ps | grep 'freeswitch' | cut -d ' ' -f 1 | xargs --no-run-if-empty docker kill || /usr/bin/true;
-docker container ls -a | grep freeswitch | cut -d ' ' -f 1 | xargs --no-run-if-empty docker container rm || /usr/bin/true;
+docker ps | grep "${IMAGE}" | cut -d ' ' -f 1 | xargs --no-run-if-empty docker kill || /usr/bin/true;
+docker container ls -a | grep "${IMAGE}" | cut -d ' ' -f 1 | xargs --no-run-if-empty docker container rm || /usr/bin/true;
 
 docker run -d --net=host \
               --privileged \
@@ -20,4 +13,4 @@ docker run -d --net=host \
               --log-opt tag="{{.Name}}" \
               --restart unless-stopped \
               -v /tmp:/tmp \
-              ${IMAGE}
+              ${IMAGE}:${VERSION}
